894606a61c4cdb00457c8dc237b6cbd96a3c8ead
adanto <adanto64@gmail.com>
2014-11-27 23:41:44 +0100

    efectivamente no se estaban añadiendo cosas porque con el test de comprobar listando todos los pacientes que han quedado en la app no ha salido nada so... aun queda pasarlo a la base de datos

--- src/logica/Aplicacion.java
+++ src/logica/Aplicacion.java
@@ -11,37 +11,11 @@ public class Aplicacion {
 		try{
 			//Se crea el Servicio de Emergencias
 			TestPacientes emergencias = new TestPacientes();
+
+			System.out.println("Empezamos en cero,");
 			// AQUÍ INCLUIR CADA UNA DE LAS EJECUCIONES
 			//--------------------------------------------------------------------
 			
-			// Primera ejecución
-			//Se busca un paciente y se crea si no existe
-			
-			if (emergencias.buscarPaciente("10123456A")==null)
-			emergencias.añadirPaciente(new Paciente("10123456A", "Juan",
-			"Martinez Gandia", "Calle Santiago, 4 Valencia", 123453250,
-			50, 'H'));
-			
-			//Se busca un paciente y si lo encuentra se muestran sus datos
-			Paciente p = emergencias.buscarPaciente("10123456A");
-			if (p!=null)
-			System.out.println(" DNI: "+p.getDni()+" Nombre: "+p.getNombre()+
-			" Apellidos: "+p.getApellidos()+
-			" Dirección: "+p.getDireccion()+" Teléf.: "+p.getTelefono()+
-			" Edad: "+p.getEdad()+" Sexo: "+p.getSexo());
-			
-			//Segunda ejecución
-			//Se busca un paciente y si lo encuentra se muestran sus datos
-			Paciente p1 = emergencias.buscarPaciente("10123456A");
-			if (p1!=null)
-			System.out.println(" DNI: "+p1.getDni()+
-			" Nombre: "+p1.getNombre()+" Apellidos: "+p1.getApellidos()+
-			" Dirección: "+p1.getDireccion()+" Teléf.: "+p1.getTelefono()+
-			" Edad: "+p1.getEdad()+" Sexo: "+p1.getSexo());
-			//Se crea un paciente
-			emergencias.añadirPaciente(new Paciente("10123457A", "Pedro",
-			"Suecaz Santos", "Calle San Vicente, 4 Valencia", 123453251, 25,
-			'H'));
 			//Se listan todos los pacientes
 			List<Paciente> listaPacientes = emergencias.ListarPacientes();
 			for(Paciente pac:listaPacientes)
