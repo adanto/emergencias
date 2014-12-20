7f883d59f64400bda07cd372d90f0fa4097412f7
adanto <adanto64@gmail.com>
2014-12-20 13:32:45 +0100

    Añadidos algunos comentarios explicativos en los métodos más extraños

--- src/logica/ServicioEmergencia.java
+++ src/logica/ServicioEmergencia.java
@@ -38,6 +38,7 @@ public List <Emergencia> listaEmergencias() throws DAOExcepcion, LogicaExcepcion
 	return emergencias;
 }
 
+//Método para obtener la ambulancia mas cercana a una posicion (x,y)
 public int ambMinima(double d, double f) throws LogicaExcepcion{
 	int amb = -1;
 	try{
@@ -47,6 +48,8 @@ public int ambMinima(double d, double f) throws LogicaExcepcion{
 	};
 	return amb;
 }
+
+//Método para obtener la ambulancia mas cercana a una emergencia
 public int ambMinima(String cod) throws LogicaExcepcion{
 	int amb = -1;
 	try{
@@ -56,6 +59,8 @@ public int ambMinima(String cod) throws LogicaExcepcion{
 	};
 	return amb;
 }
+
+//Método para obtener el hospital mas cercano a una posicion (x,y)
 public String hospMinimo(double d, double f) throws LogicaExcepcion{
 	String amb = "-1";
 	try{
@@ -67,6 +72,7 @@ public String hospMinimo(double d, double f) throws LogicaExcepcion{
 }
 
 
+//Método para obtener el hospital más cercano a una emergencia
 public String hospMinimo(String cod) throws LogicaExcepcion{
 	String amb = "-1";
 	try{
@@ -77,6 +83,35 @@ public String hospMinimo(String cod) throws LogicaExcepcion{
 	return amb;
 }
 
+/** WARNING: El código que viene a continuación te hará llorar.
+ * Este cerdito puede que le ayude a sobrellevarlo. 
+ * 
+ *                         _
+ * _._ _..._ .-',     _.._(`))
+ *'-. `     '  /-._.-'    ',/
+ *   )         \            '.
+ *  / _    _    |             \
+ * |  a    a    /              |
+ * \   .-.                     ;  
+ *  '-('' ).-'       ,'       ;
+ *     '-;           |      .'
+ *        \           \    /
+ *        | 7  .__  _.-\   \
+ *        | |  |  ``/  /`  /
+ *       /,_|  |   /,_/   /
+ *          /,_/      '`-'
+**/
+
+/**
+ * Este método añade las emergencias de la siguiente forma:
+ * 	1º Añade una emergencia con código y paciente
+ * 	2º Añade los síntomas de esa emergencia
+ * 	3º Se busca una ambulancia mínima y un hospital mínimo para añadirlos a la emergencia
+ * 	4º Una vez lo tenemos, añadimos estos 2 a el objeto emergencia
+ * 	5º Eliminamos los síntomas y la emergencia de la DB
+ * 	6º Volvemos a añdir la emergenia (esta vez con su ambulancia y hospital) y los síntomas
+ * 
+ */
 	public void anyadir(Emergencia em) throws LogicaExcepcion{
 		em.setAmb(null);
 		em.setHosp(null);
